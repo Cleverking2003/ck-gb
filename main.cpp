14175fd8a8e47e01bd8a9640c4efe62f8eba3a0e
Cleverking2003 <ananinegor@gmail.com>
2021-09-30 11:58:39 +0300

    Do not hardcode the filename

--- main.cpp
+++ main.cpp
@@ -1,8 +1,12 @@
 #include <iostream>
 #include "emul.hpp"
 
-int main() {
-    Emulator emul("tetris.gb");
+int main(int argc, char** argv) {
+    if (argc != 2) {
+        std::cout << "Usage: ck-gb <filename>\n";
+        return 1;
+    }
+    Emulator emul(argv[1]);
     while (emul.exec());
     return 0;
 }
